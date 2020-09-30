5d4918aeb207afd8dd50e2147ab4f868850a8f8a
lukas <lukas@172.17.1.2>
2020-09-29 21:19:48 -0300

    Criação da SQL para o Banco teste 01

---
+++ BancoSQL.sql
@@ -0,0 +1,24 @@
+
+CREATE TABLE public.tarefa (
+ id_tarefa SERIAL PRIMARY KEY NOT NULL,
+ data_inicial date,
+ data_entrega date,
+ titulo character varying(60) NOT NULL,
+ descricao character varying(100) NOT NULL,
+ status_entrega bit,
+ atraso bit,
+ pripridade character varying(10),
+ programada bit,
+ data_programada date,
+ 
+);
+
+CREATE TABLE public.subtarefa
+(
+   id_subtarefa SERIAL PRIMARY KEY NOT NULL,
+   nome_subtarefa character varying(50),
+   descricao_subtarefa character varying(50),
+   status_entregaSub bit,
+   fk_id_Tarefa integer,
+   FOREIGN KEY (fk_id_Tarefa) REFERENCES tarefa (id_tarefa)
+);
