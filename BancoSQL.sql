f2cf426d4ecd0ba28a1cd98428fdd8ef6296845a
lukas <lukas@172.17.1.186>
2020-10-15 22:40:08 -0300

    correção bd

--- BancoSQL.sql
+++ BancoSQL.sql
@@ -1,24 +1,21 @@
-
-CREATE TABLE public.tarefa (
+CREATE TABLE tarefa {
  id_tarefa SERIAL PRIMARY KEY NOT NULL,
+ titulo varhar (50) NOT NULL,
+ descricao varchar(100) NOT NULL,  
  data_inicial date,
  data_entrega date,
- titulo character varying(60) NOT NULL,
- descricao character varying(100) NOT NULL,
  status_entrega bit,
  atraso bit,
  pripridade character varying(10),
  programada bit,
  data_programada date,
- 
-);
+ subtarefa_id integer,
+ foreign key(subtarefa_id) REFERENCES subtarefa(id_subtarefa)
+};
 
-CREATE TABLE public.subtarefa
-(
-   id_subtarefa SERIAL PRIMARY KEY NOT NULL,
-   nome_subtarefa character varying(50),
-   descricao_subtarefa character varying(50),
-   status_entregaSub bit,
-   fk_id_Tarefa integer,
-   FOREIGN KEY (fk_id_Tarefa) REFERENCES tarefa (id_tarefa)
-);
+CREATE TABELE subtarefa {
+  id_subtarefa SERIAL PRIMARY KEY NOT NULL,
+   nome_subtarefa varchar (50),
+   descricao_subtarefa  varchar(50),
+   status_entregaSub bit
+};
